3b6d34a97d93222c455cd18dde77394016c3c473
Richard Gross <richard.gross@maibornwolff.de>
2022-12-04 16:22:11 +0100

    *** added streaming article support

--- src/articles/article-route.ts
+++ src/articles/article-route.ts
@@ -1,7 +1,12 @@
 import {FastifyInstance} from "fastify";
+import {findAllArticles} from "./article-store";
 
 export function initArticleRoute(server: FastifyInstance) {
     server.get('/articles', async (request, reply) => {
-        return 'articles\n'
+        const articles = findAllArticles();
+            // .on('data', doc => { console.log(doc); });
+            // .pipe(reply.type('json));
+        // console.log("GET articles");
+        reply.send(articles);
     });
 }
--- src/articles/article-store.ts
+++ src/articles/article-store.ts
@@ -1,4 +1,6 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, model, HydratedDocument } from 'mongoose';
+import {Stringifier, stringify} from "csv-stringify";
+import {pipeline, Readable} from "stream";
 
 export type Article = {
     _id: string;
@@ -18,4 +20,30 @@ const ActiveArticle = model<Article>('User', articleSchema);
  */
 export async function putArticle(article: Article): Promise<void> {
     await ActiveArticle.updateOne({_id: article._id}, article, {upsert: true});
+}
+
+export async function findAllArticles(): Promise<Readable> {
+    return ActiveArticle
+        .find()
+        .cursor({transform: JSON.stringify})
+        .on('data', doc => { console.log(`Store found ${doc}`); });
+}
+
+async function generateReport(){
+    let articleCursor = ActiveArticle
+        .find({active:true})
+        .cursor({transform: (article: HydratedDocument<Article>) =>{
+            const {_id:id, title} = article.toObject()
+            return {id, title}
+        }});
+
+    //create object to csv transformer
+    let csvStream = stringify({
+        header: true,
+        columns: {
+            title: 'TITLE',
+        }
+    });
+
+    const bla: Stringifier = pipeline(articleCursor, csvStream)
 }
